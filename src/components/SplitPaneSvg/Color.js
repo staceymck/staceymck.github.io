02d79bf497a95258197aae4933bc10d1f46a5b4c
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-08 13:01:25 -0500

    add styled img for collab color project

---
+++ src/components/SplitPaneSvg/Color.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import styles from './Color.module.scss';
+
+const Color = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 415.14 414.96">
+      <polygon className={styles.Cls1} points="42.08 131.06 97.3 85.67 60.09 34.88 42.08 131.06"/>
+      <polygon className={styles.Cls2} points="73.31 189.36 97.3 85.67 42.08 131.06 73.31 189.36"/>
+      <polygon className={styles.Cls1} points="57.26 289.13 138.22 314.97 53.79 330.34 57.26 289.13"/>
+      <polygon className={styles.Cls3} points="353.5 323.11 384.45 333.93 384.45 369.64 353.5 323.11"/>
+      <polygon className={styles.Cls3} points="353.31 322.78 346.73 215.43 384.38 333.64 353.31 322.78"/>
+      <polygon className={styles.Cls2} points="346.55 214.44 291.36 251.12 353.19 322.87 346.55 214.44"/>
+      <polygon className={styles.Cls5} points="300.66 188.94 291.36 251.12 346.55 214.44 300.66 188.94"/>
+      <polygon className={styles.Cls5} points="229.54 289.13 291.36 251.12 353.19 322.87 229.54 289.13"/>
+      <polygon className={styles.Cls3} points="138.54 315.01 229.33 289.32 204.47 351.39 138.54 315.01"/>
+      <polygon className={styles.Cls2} points="300.66 188.94 187.56 245.06 291.36 251.12 300.66 188.94"/>
+      <polygon className={styles.Cls6} points="187.56 245.06 229.54 289.13 291.36 251.12 187.56 245.06"/>
+      <polygon className={styles.Cls1} points="153.99 63.2 143.87 138.96 97.3 85.67 153.99 63.2"/>
+      <polygon className={styles.Cls2} points="267.73 44.33 143.87 138.96 153.99 63.2 267.73 44.33"/>
+      <polygon className={styles.Cls6} points="73.31 189.36 88.81 239.25 57.26 289.13 73.31 189.36"/>
+      <polygon className={styles.Cls2} points="143.87 138.96 88.81 239.25 73.31 189.36 143.87 138.96"/>
+      <polygon className={styles.Cls1} points="143.87 138.96 122.66 225.54 88.81 239.25 143.87 138.96"/>
+      <polygon className={styles.Cls3} points="122.81 225.46 143.9 139.36 187.35 244.87 122.81 225.46"/>
+      <polygon className={styles.Cls5} points="353.19 96.58 410.95 4.03 309.06 4.03 353.19 96.58"/>
+      <polygon className={styles.Cls5} points="267.73 44.33 309.06 4.03 353.19 96.58 267.73 44.33"/>
+      <polygon className={styles.Cls5} points="225.45 4.03 267.73 44.33 309.06 4.03 225.45 4.03"/>
+      <polygon className={styles.Cls1} points="153.99 63.2 139.19 4.03 225.45 4.03 153.99 63.2"/>
+      <polygon className={styles.Cls1} points="267.73 44.33 153.99 63.2 225.45 4.03 267.73 44.33"/>
+      <polygon className={styles.Cls3} points="60.28 34.93 138.93 4.26 97.28 85.44 60.28 34.93"/>
+      <polygon className={styles.Cls2} points="153.99 63.2 97.3 85.67 139.19 4.03 153.99 63.2"/>
+      <polygon className={styles.Cls2} points="60.09 34.88 139.19 4.03 69.95 4.03 60.09 34.88"/>
+      <polygon className={styles.Cls1} points="3.8 4.03 60.09 34.88 69.95 4.03 3.8 4.03"/>
+      <polygon className={styles.Cls2} points="353.19 322.87 344.8 354.5 384.58 370.06 353.19 322.87"/>
+      <polygon className={styles.Cls2} points="53.79 330.34 90.66 383.48 3.8 410.95 53.79 330.34"/>
+      <polygon className={styles.Cls3} points="54.13 330.62 118.99 356.89 90.68 383.29 54.13 330.62"/>
+      <polygon className={styles.Cls2} points="138.22 314.97 119.21 356.85 53.79 330.34 138.22 314.97"/>
+      <polygon className={styles.Cls1} points="204.53 351.57 155.11 350.53 138.22 314.97 204.53 351.57"/>
+      <polygon className={styles.Cls6} points="119.21 356.85 155.11 350.53 138.22 314.97 119.21 356.85"/>
+      <polygon className={styles.Cls3} points="119.42 356.94 154.92 350.69 140.87 390.33 119.42 356.94"/>
+      <polygon className={styles.Cls3} points="155.6 350.67 204.45 351.69 219.55 387.03 155.6 350.67"/>
+      <polygon className={styles.Cls2} points="229.54 289.13 230.95 354.93 204.53 351.57 229.54 289.13"/>
+      <polygon className={styles.Cls2} points="353.19 322.87 292.72 342.02 229.54 289.13 353.19 322.87"/>
+      <polygon className={styles.Cls6} points="230.95 354.93 243.03 322.87 229.54 289.13 230.95 354.93"/>
+      <polygon className={styles.Cls6} points="219.81 387.32 230.95 354.93 204.53 351.57 219.81 387.32"/>
+      <polygon className={styles.Cls5} points="292.72 342.02 243.03 322.87 229.54 289.13 292.72 342.02"/>
+      <polygon className={styles.Cls6} points="344.8 354.5 292.72 342.02 353.19 322.87 344.8 354.5"/>
+      <polygon className={styles.Cls6} points="243.03 322.87 278.45 385.67 230.95 354.93 243.03 322.87"/>
+      <polygon className={styles.Cls3} points="243.31 323.11 292.57 342.1 278.42 385.37 243.31 323.11"/>
+      <polygon className={styles.Cls2} points="292.72 342.02 315.92 375.63 278.45 385.67 292.72 342.02"/>
+      <polygon className={styles.Cls3} points="293.01 342.22 344.51 354.56 315.95 375.45 293.01 342.22"/>
+      <polygon className={styles.Cls6} points="70.04 411.26 90.66 383.48 3.8 410.95 70.04 411.26"/>
+      <polygon className={styles.Cls6} points="139.28 411.26 140.9 390.62 90.66 383.48 139.28 411.26"/>
+      <polygon className={styles.Cls3} points="70.28 411.36 90.7 383.87 138.81 411.36 70.28 411.36"/>
+      <polygon className={styles.Cls2} points="119.21 356.85 140.9 390.62 90.66 383.48 119.21 356.85"/>
+      <polygon className={styles.Cls6} points="225.54 411.26 219.81 387.32 155.11 350.53 225.54 411.26"/>
+      <polygon className={styles.Cls2} points="139.28 411.26 171.38 383.48 140.9 390.62 139.28 411.26"/>
+      <polygon className={styles.Cls6} points="225.54 411.26 139.28 411.26 171.38 383.48 225.54 411.26"/>
+      <polygon className={styles.Cls2} points="155.11 350.53 171.38 383.48 225.54 411.26 155.11 350.53"/>
+      <polygon className={styles.Cls1} points="140.9 390.62 171.38 383.48 155.11 350.53 140.9 390.62"/>
+      <polygon className={styles.Cls3} points="219.96 387.44 277.87 385.82 225.62 411.08 219.96 387.44"/>
+      <polygon className={styles.Cls2} points="230.95 354.93 219.81 387.32 278.45 385.67 230.95 354.93"/>
+      <polygon className={styles.Cls3} points="278.71 385.73 315.76 375.8 309.06 411.03 278.71 385.73"/>
+      <polygon className={styles.Cls6} points="225.54 411.26 309.14 411.26 278.45 385.67 225.54 411.26"/>
+      <polygon className={styles.Cls6} points="410.95 411.18 315.92 375.63 309.14 411.26 410.95 411.18"/>
+      <polygon className={styles.Cls6} points="384.58 370.06 315.92 375.63 344.8 354.5 384.58 370.06"/>
+      <polygon className={styles.Cls3} points="316.49 375.71 384.52 370.19 410.65 410.93 316.49 375.71"/>
+      <polygon className={styles.Cls3} points="3.97 70.34 36.68 70.08 3.97 138.91 3.97 70.34"/>
+      <polygon className={styles.Cls2} points="3.8 4.03 36.88 69.95 60.09 34.88 3.8 4.03"/>
+      <polygon className={styles.Cls1} points="3.84 70.22 3.8 4.03 36.88 69.95 3.84 70.22"/>
+      <polygon className={styles.Cls6} points="42.08 131.06 36.88 69.95 60.09 34.88 42.08 131.06"/>
+      <polygon className={styles.Cls6} points="3.84 139.46 73.31 189.36 42.08 131.06 3.84 139.46"/>
+      <polygon className={styles.Cls2} points="36.88 69.95 3.84 139.46 42.08 131.06 36.88 69.95"/>
+      <polygon className={styles.Cls2} points="3.84 225.72 73.31 189.36 3.84 139.46 3.84 225.72"/>
+      <polygon className={styles.Cls3} points="4.02 309.51 53.66 330.4 3.98 410.51 4.02 309.51"/>
+      <polygon className={styles.Cls2} points="3.84 225.72 53.79 330.34 3.84 309.33 3.84 225.72"/>
+      <polygon className={styles.Cls6} points="57.26 289.13 3.84 225.72 53.79 330.34 57.26 289.13"/>
+      <polygon className={styles.Cls3} points="4.04 225.76 73.14 189.59 57.18 288.84 4.04 225.76"/>
+      <polygon className={styles.Cls2} points="411.22 309.33 384.58 370.06 411.18 410.95 411.22 309.33"/>
+      <polygon className={styles.Cls2} points="411.22 225.72 384.58 333.84 411.22 309.33 411.22 225.72"/>
+      <polygon className={styles.Cls1} points="346.55 214.44 411.22 225.72 384.58 333.84 346.55 214.44"/>
+      <polygon className={styles.Cls6} points="384.58 370.06 384.58 333.84 411.22 309.33 384.58 370.06"/>
+      <polygon className={styles.Cls5} points="411.22 139.46 346.55 214.44 411.22 225.72 411.22 139.46"/>
+      <polygon className={styles.Cls5} points="353.19 96.58 411.22 139.46 346.55 214.44 353.19 96.58"/>
+      <polygon className={styles.Cls5} points="411.18 4.03 353.26 96.58 411.22 70.22 411.18 4.03"/>
+      <polygon className={styles.Cls5} points="411.22 139.46 353.19 96.58 411.22 70.22 411.22 139.46"/>
+      <polygon className={styles.Cls5} points="187.56 245.06 267.26 182.59 300.66 188.94 187.56 245.06"/>
+      <polygon className={styles.Cls5} points="143.87 138.96 229.54 107.03 267.73 44.33 143.87 138.96"/>
+      <polygon className={styles.Cls5} points="267.26 182.59 267.26 155.51 300.66 188.94 267.26 182.59"/>
+      <polygon className={styles.Cls3} points="97.37 85.94 143.69 138.94 73.5 189.07 97.37 85.94"/>
+      <polygon className={styles.Cls5} points="267.26 155.51 187.56 245.06 267.26 182.59 267.26 155.51"/>
+      <polygon className={styles.Cls1} points="143.87 138.96 194.75 145.2 187.56 245.06 143.87 138.96"/>
+      <polygon className={styles.Cls3} points="144.39 138.9 229.1 107.33 194.7 145.07 144.39 138.9"/>
+      <polygon className={styles.Cls5} points="267.73 44.33 229.54 107.03 254.7 116.64 267.73 44.33"/>
+      <polygon className={styles.Cls5} points="267.26 155.51 267.73 44.33 300.66 188.94 267.26 155.51"/>
+      <polygon className={styles.Cls5} points="254.7 116.64 267.26 155.51 267.73 44.33 254.7 116.64"/>
+      <polygon className={styles.Cls5} points="194.75 145.2 267.26 155.51 254.7 116.64 194.75 145.2"/>
+      <polygon className={styles.Cls5} points="229.54 107.03 194.75 145.2 254.7 116.64 229.54 107.03"/>
+      <polygon className={styles.Cls2} points="187.56 245.06 194.75 145.2 267.26 155.51 187.56 245.06"/>
+      <polygon className={styles.Cls5} points="267.73 44.33 310.46 107.03 353.19 96.58 267.73 44.33"/>
+      <polygon className={styles.Cls5} points="300.66 188.94 353.19 96.58 346.55 214.44 300.66 188.94"/>
+      <polygon className={styles.Cls5} points="310.46 107.03 300.66 188.94 353.19 96.58 310.46 107.03"/>
+      <polygon className={styles.Cls5} points="267.73 44.33 300.66 188.94 310.46 107.03 267.73 44.33"/>
+      <polygon className={styles.Cls3} points="88.92 239.31 122.11 225.9 57.93 288.31 88.92 239.31"/>
+      <polygon className={styles.Cls2} points="138.22 314.97 156.14 282.81 229.54 289.13 138.22 314.97"/>
+      <polygon className={styles.Cls2} points="122.66 225.54 156.14 282.81 187.56 245.06 122.66 225.54"/>
+      <polygon className={styles.Cls5} points="229.54 289.13 187.56 245.06 156.14 282.81 229.54 289.13"/>
+      <polygon className={styles.Cls6} points="138.22 314.97 108.59 274.14 57.26 289.13 138.22 314.97"/>
+      <polygon className={styles.Cls3} points="156.14 282.81 108.59 274.14 138.22 314.97 156.14 282.81"/>
+      <polygon className={styles.Cls2} points="122.66 225.54 108.59 274.14 57.26 289.13 122.66 225.54"/>
+      <polygon className={styles.Cls1} points="156.14 282.81 122.66 225.54 108.59 274.14 156.14 282.81"/>
+    </svg>
+  )
+}
+
+export default Color;
