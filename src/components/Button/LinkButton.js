efe3ec2cca105a2bd3bd578fd70bc4e852e8bf67
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-10 15:38:30 -0500

    rename component to reflect that it's used for links styled as buttons

--- src/components/Button/LinkButton.js
+++ src/components/Button/LinkButton.js
@@ -2,15 +2,17 @@ import React from 'react';
 import classnames from 'classnames';
 import styles from './Button.module.scss';
 
-const Button = ({text, type, wide}) => {
+const LinkButton = ({text, type, wide, link}) => {
   
   const classNames = classnames(styles.Button, {[styles[type]]: type, [styles.Wide]: wide})
 
   return (
-    <button className={classNames}>
-      {text}
-    </button>
+    <div className="test">
+      <a className={classNames} href={link}>
+         {text}
+      </a>
+    </div>
   )
 }
 
-export default Button;
+export default LinkButton;
