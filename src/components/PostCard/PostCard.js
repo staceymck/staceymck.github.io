208dcd8df1d6cb475dc64f7acf7ef70710d107b1
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-15 18:00:46 -0500

    make title of posts a link

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import Button from '../Button/Button';
+import LinkButton from '../Button/LinkButton';
 import styles from './PostCard.module.scss';
 
 const PostCard = ({title, link, teaser}) => {
   return (
     <div className={styles.PostCard}>
-      <h1>{title}</h1>
-      <p>{teaser}</p>
       <a href={link}>
-        <Button type="Primary" text="Read"/>
+        <h1>{title}</h1>
       </a>
+      <p>{teaser}</p>
+      <LinkButton type="Primary" text="Read" link={link}/>
     </div>
   )
 }
