9f944d04add021e443806b108137faebefb8f5c5
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-17 09:43:19 -0500

    update layout and styles for VolunteerLog svg

---
+++ src/components/ThemedSvg/Volunteer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Volunteer.module.scss';
+
+const Volunteer = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 415.14 414.96">
+      <rect className={styles.Cls1} x="4.07" y="3.98" width="407" height="407"/>
+      <rect className={styles.Cls2} x="34.99" y="81.66" width="345.16" height="251.63" rx="16.76"/>
+      <path className={styles.Cls3} d="M380.15,96.56c0-8.23-6.16-14.9-13.77-14.9H48.76c-7.6,0-13.77,6.67-13.77,14.9v5.62H380.15Z"/>
+      <rect className={styles.Cls4} x="47.32" y="113.67" width="320.51" height="208.06" rx="7.47"/>
+      <rect className={styles.Cls2} x="58.82" y="128.41" width="52.6" height="12.45" rx="4"/>
+      <line className={styles.Cls5} x1="58.82" y1="153.74" x2="356.32" y2="153.74"/>
+      <rect className={styles.Cls2} x="239.78" y="129.97" width="26.11" height="9.33" rx="3"/>
+      <rect className={styles.Cls2} x="269.92" y="129.97" width="26.11" height="9.33" rx="3"/>
+      <rect className={styles.Cls2} x="300.06" y="129.97" width="26.11" height="9.33" rx="3"/>
+      <rect className={styles.Cls2} x="330.21" y="129.97" width="26.11" height="9.33" rx="3"/>
+      <ellipse className={styles.Cls6} cx="104.91" cy="204.53" rx="31.29" ry="33.85"/>
+      <path className={styles.Cls3} d="M104.94,238.52a29.46,29.46,0,0,0,17.2-5.57c-1.16-6.82-5.55-7.2-5.55-7.2H92.4s-4.14.36-5.44,6.64A29.58,29.58,0,0,0,104.94,238.52Z"/>
+      <ellipse className={styles.Cls7} cx="104.6" cy="203.82" rx="15.1" ry="16.34"/>
+      <path className={styles.Cls2} d="M100,217h9.06a.32.32,0,0,1,.31.33h0v8a5,5,0,0,1-4.84,5.23h0a5,5,0,0,1-4.84-5.23h0v-8A.32.32,0,0,1,100,217Z"/>
+      <path className={styles.Cls8} d="M99.75,221.38a12.94,12.94,0,0,0,9.67,0v-1.29H99.75Z"/>
+      <ellipse className={styles.Cls2} cx="104.6" cy="207.08" rx="13.87" ry="15.01"/>
+      <path className={styles.Cls7} d="M94.72,193.24s5.85,12.84,22.34,5.38l-3.84-6.52-6.81-2.64Z"/>
+      <path className={styles.Cls7} d="M94.67,192.37a9,9,0,0,1,2-3.5c2.73-2.92,7.2-3.53,9.49-6.86a2,2,0,0,1-.82,2.74c2.2,0,4.74-.22,6-2.18a4.76,4.76,0,0,1-.7,4.7c1.95.1,4,1.52,4.16,3.63a4.34,4.34,0,0,1-1.8,3.54,9.49,9.49,0,0,1-3.62,1.47C105.66,196.8,92.25,200.5,94.67,192.37Z"/>
+      <ellipse className={styles.Cls2} cx="90.73" cy="207.06" rx="1.29" ry="2.62"/>
+      <ellipse className={styles.Cls2} cx="118.47" cy="207.06" rx="1.29" ry="2.62"/>
+      <rect className={styles.Cls2} x="74.99" y="249.01" width="60.26" height="8.15" rx="3"/>
+      <rect className={styles.Cls2} x="74.99" y="264.29" width="60.26" height="8.15" rx="3"/>
+      <rect className={styles.Cls2} x="74.99" y="279.57" width="31.07" height="8.15" rx="3"/>
+      <rect className={styles.Cls3} x="315.45" y="177.69" width="41.69" height="9.59" rx="3"/>
+      <rect className={styles.Cls2} x="152.65" y="234.6" width="181.52" height="8.23" rx="3"/>
+      <rect className={styles.Cls2} x="152.63" y="249.13" width="181.52" height="8.23" rx="3"/>
+      <rect className={styles.Cls2} x="152.63" y="263.66" width="181.52" height="8.23" rx="3"/>
+      <rect className={styles.Cls2} x="152.63" y="278.17" width="123.13" height="8.28" rx="3"/>
+      <rect className={styles.Cls2} x="152.63" y="177.69" width="71.98" height="20.45" rx="4"/>
+      <rect className={styles.Cls2} x="152.63" y="204.37" width="93.01" height="20.45" rx="4"/>
+    </svg>
+  )
+}
+
+export default Volunteer;
