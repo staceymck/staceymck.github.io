70be61d108e1d6a3a2f3cf399ff4efe06a171409
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-18 12:03:10 -0500

    remove paths causing thicker lines

--- src/components/ThemedSvg/Color.js
+++ src/components/ThemedSvg/Color.js
@@ -9,14 +9,11 @@ const Color = () => {
       <polygon className={styles.Cls3} points="99.34 192.65 118.69 109.03 74.16 145.63 99.34 192.65"/>
       <polygon className={styles.Cls2} points="86.4 273.11 151.69 293.95 83.6 306.34 86.4 273.11"/>
       <polygon className={styles.Cls4} points="325.37 300.56 350.25 309.25 350.25 337.96 325.37 300.56"/>
-      <path className={styles.Cls5} d="M325.68,300.8l24.44,8.54v28.2L325.68,300.8m-.62-.49,25.31,38.06V309.16l-25.31-8.85Z"/>
       <polygon className={styles.Cls4} points="325.18 300.22 319.89 213.86 350.18 308.96 325.18 300.22"/>
-      <path className={styles.Cls5} d="M320.07,214.86,350,308.76l-24.69-8.63-5.22-85.27m-.37-2,5.36,87.44,25.31,8.85L319.7,212.87Z"/>
       <polygon className={styles.Cls3} points="319.7 212.87 275.2 242.45 325.06 300.31 319.7 212.87"/>
       <polygon className={styles.Cls6} points="282.7 192.31 275.2 242.45 319.7 212.87 282.7 192.31"/>
       <polygon className={styles.Cls6} points="225.34 273.11 275.2 242.45 325.06 300.31 225.34 273.11"/>
       <polygon className={styles.Cls4} points="152.02 293.98 225.13 273.3 205.11 323.28 152.02 293.98"/>
-      <path className={styles.Cls5} d="M224.92,273.49,205.05,323.1,152.34,294l72.58-20.53m.42-.38L151.69,294l53.48,29.51,20.17-50.35Z"/>
       <polygon className={styles.Cls3} points="282.7 192.31 191.48 237.56 275.2 242.45 282.7 192.31"/>
       <polygon className={styles.Cls7} points="191.48 237.56 225.34 273.11 275.2 242.45 191.48 237.56"/>
       <polygon className={styles.Cls2} points="164.41 90.9 156.25 152 118.69 109.03 164.41 90.9"/>
@@ -25,28 +22,23 @@ const Color = () => {
       <polygon className={styles.Cls3} points="156.25 152 111.84 232.88 99.34 192.65 156.25 152"/>
       <polygon className={styles.Cls2} points="156.25 152 139.14 221.82 111.84 232.88 156.25 152"/>
       <polygon className={styles.Cls4} points="139.29 221.74 156.28 152.4 191.27 237.37 139.29 221.74"/>
-      <path className={styles.Cls5} d="M156.31,152.8l34.74,84.37-51.61-15.52,16.87-68.85m-.06-.8-17.11,69.83,52.34,15.74L156.25,152Z"/>
       <polygon className={styles.Cls6} points="325.06 117.82 371.64 43.18 289.47 43.18 325.06 117.82"/>
       <polygon className={styles.Cls6} points="256.13 75.68 289.47 43.18 325.06 117.82 256.13 75.68"/>
       <polygon className={styles.Cls6} points="222.04 43.18 256.13 75.68 289.47 43.18 222.04 43.18"/>
       <polygon className={styles.Cls2} points="164.41 90.9 152.48 43.18 222.04 43.18 164.41 90.9"/>
       <polygon className={styles.Cls2} points="256.13 75.68 164.41 90.9 222.04 43.18 256.13 75.68"/>
       <polygon className={styles.Cls4} points="88.88 68.12 152.22 43.42 118.67 108.79 88.88 68.12"/>
-      <path className={styles.Cls5} d="M152,43.65l-33.29,64.9L89.07,68.17,152,43.65m.53-.47L88.68,68.06l30,41,33.79-65.85Z"/>
       <polygon className={styles.Cls3} points="164.41 90.9 118.69 109.03 152.48 43.18 164.41 90.9"/>
       <polygon className={styles.Cls3} points="88.68 68.06 152.48 43.18 96.64 43.18 88.68 68.06"/>
       <polygon className={styles.Cls2} points="43.29 43.18 88.68 68.06 96.64 43.18 43.29 43.18"/>
       <polygon className={styles.Cls3} points="325.06 300.31 318.29 325.82 350.37 338.37 325.06 300.31"/>
       <polygon className={styles.Cls3} points="83.6 306.34 113.34 349.19 43.29 371.35 83.6 306.34"/>
       <polygon className={styles.Cls4} points="83.94 306.62 136.14 327.76 113.36 349 83.94 306.62"/>
-      <path className={styles.Cls5} d="M84.28,306.89l51.63,20.91-22.53,21-29.1-41.92m-.68-.55,29.74,42.85,23-21.47L83.6,306.34Z"/>
       <polygon className={styles.Cls3} points="151.69 293.95 136.37 327.72 83.6 306.34 151.69 293.95"/>
       <polygon className={styles.Cls2} points="205.17 323.46 165.31 322.62 151.69 293.95 205.17 323.46"/>
       <polygon className={styles.Cls7} points="136.37 327.72 165.31 322.62 151.69 293.95 136.37 327.72"/>
       <polygon className={styles.Cls4} points="136.57 327.81 165.12 322.79 153.82 354.67 136.57 327.81"/>
-      <path className={styles.Cls5} d="M164.93,322.94l-11.14,31.45-17-26.49,28.15-5m.38-.32-28.95,5.1L153.86,355l11.45-32.33Z"/>
       <polygon className={styles.Cls4} points="165.8 322.76 205.09 323.58 217.23 352 165.8 322.76"/>
-      <path className={styles.Cls5} d="M166.3,322.89l38.71.82,12,28L166.3,322.89m-1-.27,52.18,29.67-12.32-28.83-39.86-.84Z"/>
       <polygon className={styles.Cls3} points="225.34 273.11 226.48 326.17 205.17 323.46 225.34 273.11"/>
       <polygon className={styles.Cls3} points="325.06 300.31 276.29 315.76 225.34 273.11 325.06 300.31"/>
       <polygon className={styles.Cls7} points="226.48 326.17 236.22 300.31 225.34 273.11 226.48 326.17"/>
@@ -55,14 +47,11 @@ const Color = () => {
       <polygon className={styles.Cls7} points="318.29 325.82 276.29 315.76 325.06 300.31 318.29 325.82"/>
       <polygon className={styles.Cls7} points="236.22 300.31 264.78 350.96 226.48 326.17 236.22 300.31"/>
       <polygon className={styles.Cls4} points="236.5 300.56 276.14 315.83 264.75 350.66 236.5 300.56"/>
-      <path className={styles.Cls5} d="M236.78,300.8,276,315.91l-11.26,34.44L236.78,300.8m-.56-.49L264.78,351l11.51-35.2-40.07-15.45Z"/>
       <polygon className={styles.Cls3} points="276.29 315.76 295 342.87 264.78 350.96 276.29 315.76"/>
       <polygon className={styles.Cls4} points="276.58 315.96 318 325.88 295.03 342.69 276.58 315.96"/>
-      <path className={styles.Cls5} d="M276.87,316.16l40.84,9.78-22.65,16.57-18.19-26.35m-.58-.4L295,342.86l23.29-17-42-10.06Z"/>
       <polygon className={styles.Cls7} points="96.7 371.6 113.34 349.19 43.29 371.35 96.7 371.6"/>
       <polygon className={styles.Cls7} points="152.54 371.6 153.86 354.95 113.34 349.19 152.54 371.6"/>
       <polygon className={styles.Cls4} points="96.95 371.65 113.37 349.54 152.07 371.65 96.95 371.65"/>
-      <path className={styles.Cls5} d="M113.41,349.7l38.19,21.83H97.2l16.21-21.83m-.07-.33L96.7,371.78h55.84l-39.2-22.41Z"/>
       <polygon className={styles.Cls3} points="136.37 327.72 153.86 354.95 113.34 349.19 136.37 327.72"/>
       <polygon className={styles.Cls7} points="222.11 371.6 217.49 352.29 165.31 322.62 222.11 371.6"/>
       <polygon className={styles.Cls3} points="152.54 371.6 178.43 349.19 153.86 354.95 152.54 371.6"/>
@@ -70,17 +59,13 @@ const Color = () => {
       <polygon className={styles.Cls3} points="165.31 322.62 178.43 349.19 222.11 371.6 165.31 322.62"/>
       <polygon className={styles.Cls2} points="153.86 354.95 178.43 349.19 165.31 322.62 153.86 354.95"/>
       <polygon className={styles.Cls4} points="217.65 352.41 264.2 351.11 222.19 371.42 217.65 352.41"/>
-      <path className={styles.Cls5} d="M263.62,351.25l-41.34,20-4.48-18.71,45.82-1.28m1.16-.29-47.29,1.33,4.62,19.31L264.78,351Z"/>
       <polygon className={styles.Cls3} points="226.48 326.17 217.49 352.29 264.78 350.96 226.48 326.17"/>
       <polygon className={styles.Cls4} points="265.05 351.02 294.84 343.04 289.45 371.37 265.05 351.02"/>
-      <path className={styles.Cls5} d="M294.68,343.21l-5.31,27.93-24.06-20.06,29.37-7.87m.32-.35L264.78,351l24.76,20.64L295,342.86Z"/>
       <polygon className={styles.Cls7} points="222.11 371.6 289.54 371.6 264.78 350.96 222.11 371.6"/>
       <polygon className={styles.Cls7} points="371.64 371.53 295 342.87 289.54 371.6 371.64 371.53"/>
       <polygon className={styles.Cls7} points="350.37 338.37 295 342.87 318.29 325.82 350.37 338.37"/>
       <polygon className={styles.Cls4} points="295.57 342.94 350.31 338.5 371.33 371.28 295.57 342.94"/>
-      <path className={styles.Cls5} d="M350.24,338.63,371,371l-74.89-28,54.1-4.39m.13-.26L295,342.86l76.64,28.67-21.27-33.16Z"/>
       <polygon className={styles.Cls4} points="43.45 96.69 69.76 96.47 43.45 151.85 43.45 96.69"/>
-      <path className={styles.Cls5} d="M69.57,96.6l-26,54.69V96.81l26-.21m.39-.25-26.64.21V152.4L70,96.35Z"/>
       <polygon className={styles.Cls3} points="43.29 43.18 69.96 96.35 88.68 68.06 43.29 43.18"/>
       <polygon className={styles.Cls2} points="43.32 96.56 43.29 43.18 69.96 96.35 43.32 96.56"/>
       <polygon className={styles.Cls7} points="74.16 145.63 69.96 96.35 88.68 68.06 74.16 145.63"/>
@@ -88,11 +73,9 @@ const Color = () => {
       <polygon className={styles.Cls3} points="69.96 96.35 43.32 152.4 74.16 145.63 69.96 96.35"/>
       <polygon className={styles.Cls3} points="43.32 221.97 99.34 192.65 43.32 152.4 43.32 221.97"/>
       <polygon className={styles.Cls4} points="43.49 289.58 83.46 306.4 43.46 370.91 43.49 289.58"/>
-      <path className={styles.Cls5} d="M43.61,289.77l39.67,16.69-39.7,64,0-80.7m-.25-.38,0,82,40.31-65L43.36,289.39Z"/>
       <polygon className={styles.Cls3} points="43.32 221.97 83.6 306.34 43.32 289.39 43.32 221.97"/>
       <polygon className={styles.Cls7} points="86.4 273.11 43.32 221.97 83.6 306.34 86.4 273.11"/>
       <polygon className={styles.Cls4} points="43.52 222 99.18 192.87 86.32 272.82 43.52 222"/>
-      <path className={styles.Cls5} d="M99,193.1,86.24,272.53,43.71,222,99,193.1m.32-.46L43.32,222,86.4,273.11l12.94-80.47Z"/>
       <polygon className={styles.Cls3} points="371.85 289.39 350.37 338.37 371.82 371.35 371.85 289.39"/>
       <polygon className={styles.Cls3} points="371.85 221.97 350.37 309.16 371.85 289.39 371.85 221.97"/>
       <polygon className={styles.Cls2} points="319.7 212.87 371.85 221.97 350.37 309.16 319.7 212.87"/>
@@ -105,11 +88,9 @@ const Color = () => {
       <polygon className={styles.Cls6} points="156.25 152 225.34 126.25 256.13 75.68 156.25 152"/>
       <polygon className={styles.Cls6} points="255.76 187.18 255.76 165.34 282.7 192.31 255.76 187.18"/>
       <polygon className={styles.Cls4} points="118.76 109.29 156.06 151.98 99.54 192.35 118.76 109.29"/>
-      <path className={styles.Cls5} d="M118.83,109.56,155.88,152,99.74,192.06l19.09-82.5m-.14-.53L99.34,192.64,156.25,152l-37.56-43Z"/>
       <polygon className={styles.Cls6} points="255.76 165.34 191.48 237.56 255.76 187.18 255.76 165.34"/>
       <polygon className={styles.Cls2} points="156.25 152 197.28 157.03 191.48 237.56 156.25 152"/>
       <polygon className={styles.Cls4} points="156.77 151.94 224.9 126.55 197.23 156.9 156.77 151.94"/>
-      <path className={styles.Cls5} d="M224.46,126.85l-27.28,29.92-39.88-4.9,67.16-25m.88-.6L156.25,152l41,5,28.06-30.78Z"/>
       <polygon className={styles.Cls6} points="256.13 75.68 225.34 126.25 245.63 134 256.13 75.68"/>
       <polygon className={styles.Cls6} points="255.76 165.34 256.13 75.68 282.7 192.31 255.76 165.34"/>
       <polygon className={styles.Cls6} points="245.63 134 255.76 165.34 256.13 75.68 245.63 134"/>
@@ -121,7 +102,6 @@ const Color = () => {
       <polygon className={styles.Cls6} points="290.6 126.25 282.7 192.31 325.06 117.82 290.6 126.25"/>
       <polygon className={styles.Cls6} points="256.13 75.68 282.7 192.31 290.6 126.25 256.13 75.68"/>
       <polygon className={styles.Cls4} points="111.95 232.94 138.6 222.18 87.07 272.28 111.95 232.94"/>
-      <path className={styles.Cls5} d="M138.05,222.54,87.74,271.46,112,233.08l26-10.54m1.09-.71-27.3,11L86.4,273.11l52.74-51.28Z"/>
       <polygon className={styles.Cls3} points="151.69 293.95 166.14 268.01 225.34 273.11 151.69 293.95"/>
       <polygon className={styles.Cls3} points="139.14 221.82 166.14 268.01 191.48 237.56 139.14 221.82"/>
       <polygon className={styles.Cls6} points="225.34 273.11 191.48 237.56 166.14 268.01 225.34 273.11"/>
