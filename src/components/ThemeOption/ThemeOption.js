4174c020dc17049d07534368d2a1fe87b92b5e1b
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-19 15:26:06 -0500

    handle theme option clicks

--- src/components/ThemeOption/ThemeOption.js
+++ src/components/ThemeOption/ThemeOption.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './ThemeOption.module.scss';
 
-const ThemeOption = ({color}) => {
+const ThemeOption = ({onClick, color}) => {
   return (
-    <button aria-label={color} id={color} className={`${styles.Option} + ${styles[color]}`}></button>
+    <button onClick={() => onClick(color)} aria-label={color} id={color} className={`${styles.Option} + ${styles[color]}`}></button>
   )
 }
 
