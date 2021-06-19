95333cfd77013cff4c617353c48a302d2d26822e
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-19 15:26:53 -0500

    use themed svg as hero section graphic

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import Graphic from '../../assets/hero.svg';
+import Main from '../ThemedSvg/Main';
 import styles from './Hero.module.scss';
 
 const Hero = () => {
   return (
     <div className={styles.Hero}>
       <div className={styles.Left}>
-        <h1>Fullstack developer</h1>
-        <h2>Hi, I'm Stacey McKnight, a web developer with a background in graphic design and marketing</h2>
+        <h1>Full stack developer</h1>
+        <h2>Hi! I'm Stacey McKnight, a web developer with a background in graphic design and marketing</h2>
       </div>
       <div className={styles.Right}>
-        <img src={Graphic} />
+        <Main />
       </div>
     </div>
   )
