a6629b41c5fc4d4a511310af54f8f586fdcde877
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-20 10:27:59 -0500

    remove text content from footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,6 @@ import styles from './Footer.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.Footer}>
-      <h1>PS:</h1>
-      <p>Have you tried another color scheme? Check out the buttons in the top menu</p>
       <div className={styles.IconBlock}>
          <a href="https://www.linkedin.com/in/staceymcknight314/" aria-label="Visit LinkedIn profile">
              <i className="fa fa-linkedin" aria-hidden="true"></i>
