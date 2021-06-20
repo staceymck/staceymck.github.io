2e035d31e9423867d4cc03e7d56d44b05372af73
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-20 10:36:46 -0500

    add links to page sections

--- src/components/NavLinks/NavLinks.js
+++ src/components/NavLinks/NavLinks.js
@@ -4,18 +4,19 @@ import styles from './NavLinks.module.scss';
 
 const NavLinks = ({open, handleOpen}) => {
 
-  const navLinksClassNames = classnames(styles.NavLinks, {[styles.Open]: open} )
+  const navLinksClassNames = classnames(styles.NavLinks, {[styles.Open]: open})
+
   return (
     <nav className={styles.Nav}>
       <ul className={navLinksClassNames} onClick={() => handleOpen(false)}>
         <li className={styles.NavLink}>
-          <a>About</a>
+          <a href="#about">About</a>
         </li>
         <li className={styles.NavLink} onClick={() => handleOpen(false)}>
-          <a>Projects</a>
+          <a href="#projects">Projects</a>
         </li>
         <li className={styles.NavLink} onClick={() => handleOpen(false)}>
-          <a>Posts</a>
+          <a href="#posts">Posts</a>
         </li>
       </ul>
     </nav>
