ec6eb3260b7d46223a6e9d0c28828a09f789d814
Stacey McKnight <mcknight.stm@gmail.com>
2021-06-21 16:28:39 -0500

    open project modal on project card click

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './ProjectCard.module.scss';
 
-const ProjectCard = ({img, alt, name, tech}) => {
+const ProjectCard = ({img, name, tech, onClick}) => {
 
   return (
-    <div className={styles.ProjectCard}>
-      <div className={styles.Img}>
-        {img}
-      </div>
-      <div className={styles.Text}>
-        <h1>{name}</h1>
-        <h2>{tech}</h2>
-      </div>
+    <div className={styles.ProjectCard} onClick={onClick}>
+        <div className={styles.Img}>
+          {img}
+        </div>
+        <div className={styles.Text}>
+          <h1>{name}</h1>
+          <h2>{tech}</h2>
+        </div>
     </div>
   )
 }
