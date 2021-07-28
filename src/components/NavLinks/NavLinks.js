a36fdaf271b910183fab1ce4f5841d5f3a526b88
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-28 11:18:54 -0500

    remove unused scroll var

--- src/components/NavLinks/NavLinks.js
+++ src/components/NavLinks/NavLinks.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import styles from './NavLinks.module.scss';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 const NavLinks = ({open, handleOpen}) => {
 
